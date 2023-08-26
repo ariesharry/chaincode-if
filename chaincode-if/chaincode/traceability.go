6b1e91d44ec6c2b4a2e00ce23189e6dc4cfa674b
ariesharry <ariesharry3@gmail.com>
2023-08-26 09:14:17 +0700

    update traceability

--- chaincode-if/chaincode/traceability.go
+++ chaincode-if/chaincode/traceability.go
@@ -8,6 +8,13 @@ import (
 )
 
 // Commodity represents the structure for a commodity
+type Traceability struct {
+	ID       string   `json:"id"`
+	Status   []string `json:"status"`
+	Location []string `json:"location"`
+	PIC      []string `json:"pic"`
+}
+
 type Commodity struct {
 	ID           string       `json:"id"`
 	Name         string       `json:"name"`
@@ -16,29 +23,20 @@ type Commodity struct {
 	Traceability Traceability `json:"traceability"`
 }
 
-// Traceability represents the traceability structure for a commodity
-type Traceability struct {
-	ID      string   `json:"id"`
-	Status  []string `json:"status"`
-	Location []string `json:"location"`
-	PIC     []string `json:"pic"`
-}
-
-// ProcessedCommodity represents the structure for a processed commodity
 type ProcessedCommodity struct {
-	ID         string    `json:"id"`
-	Quantity   float64   `json:"quantity"`
-	Material   []string  `json:"material"`
-	BatchNumber string   `json:"batchNumber"`
-	Quality    string    `json:"quality"`
+	ID         string   `json:"id"`
+	Quantity   float64  `json:"quantity"`
+	Material   []string `json:"material"`
+	BatchNumber string  `json:"batchNumber"`
+	Quality    string   `json:"quality"`
 }
 
-func (pc *PalmOilContract) Harvest(ctx contractapi.TransactionContextInterface, commodityID string, name string, quantity float64, dateHarvested string, traceabilityID string) error {
+func (pc *PalmOilContract) Harvest(ctx contractapi.TransactionContextInterface, commodityID string, name string, quantity float64, dateHarvested string, traceabilityID string, pic string, location string) error {
 	traceability := Traceability{
 		ID: traceabilityID,
 		Status: []string{"harvested"},
-		Location: []string{},
-		PIC: []string{},
+		Location: []string{location},
+		PIC: []string{pic},
 	}
 
 	commodity := Commodity{
@@ -57,7 +55,9 @@ func (pc *PalmOilContract) Harvest(ctx contractapi.TransactionContextInterface, 
 	return ctx.GetStub().PutState(commodityID, commodityJSON)
 }
 
-func (pc *PalmOilContract) Collect(ctx contractapi.TransactionContextInterface, commodityID string) error {
+
+func (pc *PalmOilContract) Collect(ctx contractapi.TransactionContextInterface, commodityID string, pic string, location string) error {
+	// Fetch the commodity data from the ledger
 	commodityJSON, err := ctx.GetStub().GetState(commodityID)
 	if err != nil {
 		return fmt.Errorf("failed to read from world state: %v", err)
@@ -67,70 +67,29 @@ func (pc *PalmOilContract) Collect(ctx contractapi.TransactionContextInterface, 
 	}
 
 	var commodity Commodity
-	json.Unmarshal(commodityJSON, &commodity)
+	err = json.Unmarshal(commodityJSON, &commodity)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal commodity JSON: %v", err)
+	}
 
-	// Update the traceability's status
+	// Update the traceability's status, location, and PIC
 	commodity.Traceability.Status = append(commodity.Traceability.Status, "collected")
-
-	commodityJSON, err = json.Marshal(commodity)
-	if err != nil {
-		return err
-	}
-
-	return ctx.GetStub().PutState(commodityID, commodityJSON)
-}
-
-func (pc *PalmOilContract) Transport(ctx contractapi.TransactionContextInterface, commodityID string) error {
-	commodityJSON, err := ctx.GetStub().GetState(commodityID)
-	if err != nil {
-		return fmt.Errorf("failed to read from world state: %v", err)
-	}
-	if commodityJSON == nil {
-		return fmt.Errorf("the commodity with ID %s does not exist", commodityID)
-	}
-
-	var commodity Commodity
-	json.Unmarshal(commodityJSON, &commodity)
-
-	// Update the traceability's status
-	commodity.Traceability.Status = append(commodity.Traceability.Status, "in transport")
-
-	commodityJSON, err = json.Marshal(commodity)
-	if err != nil {
-		return err
-	}
-
-	return ctx.GetStub().PutState(commodityID, commodityJSON)
-}
-
-func (pc *PalmOilContract) UpdateTransport(ctx contractapi.TransactionContextInterface, commodityID string, location string, hasArrived bool) error {
-	commodityJSON, err := ctx.GetStub().GetState(commodityID)
-	if err != nil {
-		return fmt.Errorf("failed to read from world state: %v", err)
-	}
-	if commodityJSON == nil {
-		return fmt.Errorf("the commodity with ID %s does not exist", commodityID)
-	}
-
-	var commodity Commodity
-	json.Unmarshal(commodityJSON, &commodity)
-
-	// Update the traceability's location
+	commodity.Traceability.PIC = append(commodity.Traceability.PIC, pic)
 	commodity.Traceability.Location = append(commodity.Traceability.Location, location)
 
-	if hasArrived {
-		commodity.Traceability.Status = append(commodity.Traceability.Status, "delivered")
-	}
-
+	// Marshal the updated commodity back to JSON
 	commodityJSON, err = json.Marshal(commodity)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to marshal commodity: %v", err)
 	}
 
+	// Update the commodity in the ledger
 	return ctx.GetStub().PutState(commodityID, commodityJSON)
 }
 
-func (pc *PalmOilContract) Process(ctx contractapi.TransactionContextInterface, commodityID string, processedID string, quantity float64, material []string, batchNumber string, quality string) error {
+
+func (pc *PalmOilContract) Transport(ctx contractapi.TransactionContextInterface, commodityID string, pic string, location string) error {
+	// Fetch the commodity data from the ledger
 	commodityJSON, err := ctx.GetStub().GetState(commodityID)
 	if err != nil {
 		return fmt.Errorf("failed to read from world state: %v", err)
@@ -140,17 +99,92 @@ func (pc *PalmOilContract) Process(ctx contractapi.TransactionContextInterface, 
 	}
 
 	var commodity Commodity
-	json.Unmarshal(commodityJSON, &commodity)
+	err = json.Unmarshal(commodityJSON, &commodity)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal commodity JSON: %v", err)
+	}
 
-	// Update the traceability's status
-	commodity.Traceability.Status = append(commodity.Traceability.Status, "processed")
+	// Update the traceability's status, location, and PIC
+	commodity.Traceability.Status = append(commodity.Traceability.Status, "in transport")
+	commodity.Traceability.PIC = append(commodity.Traceability.PIC, pic)
+	commodity.Traceability.Location = append(commodity.Traceability.Location, location)
 
+	// Marshal the updated commodity back to JSON
 	commodityJSON, err = json.Marshal(commodity)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to marshal commodity: %v", err)
 	}
-	ctx.GetStub().PutState(commodityID, commodityJSON)
 
+	// Update the commodity in the ledger
+	return ctx.GetStub().PutState(commodityID, commodityJSON)
+}
+
+
+func (pc *PalmOilContract) Transported(ctx contractapi.TransactionContextInterface, commodityID string, location string, pic string) error {
+	// Fetch the commodity data from the ledger
+	commodityJSON, err := ctx.GetStub().GetState(commodityID)
+	if err != nil {
+		return fmt.Errorf("failed to read from world state: %v", err)
+	}
+	if commodityJSON == nil {
+		return fmt.Errorf("the commodity with ID %s does not exist", commodityID)
+	}
+
+	var commodity Commodity
+	err = json.Unmarshal(commodityJSON, &commodity)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal commodity JSON: %v", err)
+	}
+
+	// Update the traceability's status, location, and PIC
+	commodity.Traceability.Status = append(commodity.Traceability.Status, "delivered")
+	commodity.Traceability.PIC = append(commodity.Traceability.PIC, pic)
+	commodity.Traceability.Location = append(commodity.Traceability.Location, location)
+
+	// Marshal the updated commodity back to JSON
+	commodityJSON, err = json.Marshal(commodity)
+	if err != nil {
+		return fmt.Errorf("failed to marshal commodity: %v", err)
+	}
+
+	// Update the commodity in the ledger
+	return ctx.GetStub().PutState(commodityID, commodityJSON)
+}
+
+
+func (pc *PalmOilContract) Process(ctx contractapi.TransactionContextInterface, commodityID string, processedID string, quantity float64, material []string, batchNumber string, quality string, pic string, location string) error {
+	// Fetch the commodity data from the ledger
+	commodityJSON, err := ctx.GetStub().GetState(commodityID)
+	if err != nil {
+		return fmt.Errorf("failed to read from world state: %v", err)
+	}
+	if commodityJSON == nil {
+		return fmt.Errorf("the commodity with ID %s does not exist", commodityID)
+	}
+
+	var commodity Commodity
+	err = json.Unmarshal(commodityJSON, &commodity)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal commodity JSON: %v", err)
+	}
+
+	// Update the traceability's status, location, and PIC for the commodity
+	commodity.Traceability.Status = append(commodity.Traceability.Status, "processed")
+	commodity.Traceability.PIC = append(commodity.Traceability.PIC, pic)
+	commodity.Traceability.Location = append(commodity.Traceability.Location, location)
+
+	// Marshal the updated commodity back to JSON
+	commodityJSON, err = json.Marshal(commodity)
+	if err != nil {
+		return fmt.Errorf("failed to marshal commodity: %v", err)
+	}
+	// Update the commodity in the ledger
+	err = ctx.GetStub().PutState(commodityID, commodityJSON)
+	if err != nil {
+		return fmt.Errorf("failed to update commodity in ledger: %v", err)
+	}
+
+	// Create a new processed commodity
 	processedCommodity := ProcessedCommodity{
 		ID:         processedID,
 		Quantity:   quantity,
@@ -161,8 +195,94 @@ func (pc *PalmOilContract) Process(ctx contractapi.TransactionContextInterface, 
 
 	processedJSON, err := json.Marshal(processedCommodity)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to marshal processed commodity: %v", err)
 	}
 
+	// Add the processed commodity to the ledger
 	return ctx.GetStub().PutState(processedID, processedJSON)
+}
+
+
+// QueryCommodityAndTraceabilityByID retrieves a commodity and its traceability by commodity ID from the ledger
+func (pc *PalmOilContract) QueryCommodityByID(ctx contractapi.TransactionContextInterface, commodityID string) (*Commodity, *Traceability, error) {
+	commodityJSON, err := ctx.GetStub().GetState(commodityID)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to read from world state: %v", err)
+	}
+	if commodityJSON == nil {
+		return nil, nil, fmt.Errorf("the commodity with ID %s does not exist", commodityID)
+	}
+
+	var commodity Commodity
+	json.Unmarshal(commodityJSON, &commodity)
+
+	traceabilityJSON, err := ctx.GetStub().GetState(commodity.Traceability.ID)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to read traceability from world state: %v", err)
+	}
+	if traceabilityJSON == nil {
+		return nil, nil, fmt.Errorf("the traceability with ID %s does not exist", commodity.Traceability.ID)
+	}
+
+	var traceability Traceability
+	json.Unmarshal(traceabilityJSON, &traceability)
+
+	return &commodity, &traceability, nil
+}
+
+// QueryAllCommoditiesAndTraceabilities retrieves all commodities and their traceabilities from the ledger
+func (pc *PalmOilContract) QueryAllCommodities(ctx contractapi.TransactionContextInterface) ([]*Commodity, []*Traceability, error) {
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+		return nil, nil, err
+	}
+	defer resultsIterator.Close()
+
+	var commodities []*Commodity
+	var traceabilities []*Traceability
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		var commodity Commodity
+		json.Unmarshal(queryResponse.Value, &commodity)
+		commodities = append(commodities, &commodity)
+
+		traceabilityJSON, err := ctx.GetStub().GetState(commodity.Traceability.ID)
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to read traceability from world state: %v", err)
+		}
+		if traceabilityJSON != nil {
+			var traceability Traceability
+			json.Unmarshal(traceabilityJSON, &traceability)
+			traceabilities = append(traceabilities, &traceability)
+		}
+	}
+
+	return commodities, traceabilities, nil
+}
+
+// QueryAllProcessedCommodities retrieves all processed commodities from the ledger
+func (pc *PalmOilContract) QueryAllProcessedCommodities(ctx contractapi.TransactionContextInterface) ([]*ProcessedCommodity, error) {
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	var processedCommodities []*ProcessedCommodity
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		var processedCommodity ProcessedCommodity
+		json.Unmarshal(queryResponse.Value, &processedCommodity)
+		processedCommodities = append(processedCommodities, &processedCommodity)
+	}
+
+	return processedCommodities, nil
 }
